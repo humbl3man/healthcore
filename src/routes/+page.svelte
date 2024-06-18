24b0c4ed420ac1eaa114cf34d4c2263f910e7944
Konstantin <humbl3dev@gmail.com>
2024-06-18 11:22:32 -0700

    remove boilerplate html from homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex h-screen w-screen flex-col items-center justify-center">
+	<h1 class="font-bold text-4xl">HealthCore - Fitness Tracker</h1>
+</div>
